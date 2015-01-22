f7cca11dcf1e4ff1472a379131f3954d185a6a1e
Karl Williamson <khw@cpan.org>
2015-01-22 14:03:40 -0700

    XXX cpan/Pod-Simple/lib/Pod/Simple/HTML.pm: merge

--- cpan/Pod-Simple/lib/Pod/Simple/HTML.pm
+++ cpan/Pod-Simple/lib/Pod/Simple/HTML.pm
@@ -654,7 +654,7 @@ sub do_pod_link {
         $self->resolve_pod_page_link($to, $section);
          # (I pass it the section value, but I don't see a
          #  particular reason it'd use it.)
-      DEBUG > 1 and print STDERR "resolve_pod_page_link gives ", $to || "(nil)", "\n";
+      DEBUG > 1 and print STDERR "resolve_pod_page_link gives ", $there || "(nil)", "\n";
       unless( defined $there and length $there ) {
         DEBUG and print STDERR "Can't resolve $to\n";
         return undef;
@@ -695,7 +695,11 @@ sub section_name_tidy {
   $section =~ s/^\s+//;
   $section =~ s/\s+$//;
   $section =~ tr/ /_/;
-  $section =~ tr/\x00-\x1F\x80-\x9F//d if 'A' eq chr(65); # drop crazy characters
+  if ($[ ge 5.006) {
+    $section =~ s/[[:cntrl:]][:^ascii:]//g; # drop crazy characters
+  } elsif ('A' eq chr(65)) { # But not on early EBCDIC
+    $section =~ tr/\x00-\x1F\x80-\x9F//d;
+  }
   $section = $self->unicode_escape_url($section);
   $section = '_' unless length $section;
   return $section;
